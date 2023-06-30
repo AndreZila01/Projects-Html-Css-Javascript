11418cf79ae871f613346c4e0a9535f8666c5bf2
Andrezila01 <andrecustodiotablet@gmail.com>
2023-06-30 23:56:30 +0100

    start again project

--- Projeto Hospital/Hospital_Site/Hospital_Site/aspx/LoginS.aspx.cs
+++ Projeto Hospital/Hospital_Site/Hospital_Site/aspx/LoginS.aspx.cs
@@ -1,8 +1,12 @@
 ﻿using Hospital_Site.Class;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,10 +18,68 @@ namespace Hospital_Site.aspx
 		{
 
 		}
-		protected void getcode_Click(object sender, EventArgs e)
+        [WebMethod]
+        public async static Task<string> checkUser(string username, string password)
+        {
+            var client = new RestClient($"https://localhost:44391/api/login");
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            //([FromHeader] string username, [FromHeader] string pass)
+            request.AddHeader("username", $"{Data.Encrypt(username)}");
+            request.AddHeader("pass", $"{Data.Encrypt(password)}");
+            request.RequestFormat = DataFormat.Json;
+            var response = await client.ExecuteGetAsync(request);
+            Console.WriteLine("" + response);
+
+            return response.Content;
+        }
+        protected void getcode_Click(object sender, EventArgs e)
 		{
 			var brain = new Brain();
 			brain.sendemail("", "");
-		}
+
+            try
+            {
+
+                SmtpClient mySmtpClient = new SmtpClient("my.smtp.exampleserver.net");
+
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                   System.Net.NetworkCredential("username", "password");
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress("test@example.com", "TestFromName");
+                MailAddress to = new MailAddress("test2@example.com", "TestToName");
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
+
+                // add ReplyTo
+                MailAddress replyTo = new MailAddress("reply@example.com");
+                myMail.ReplyToList.Add(replyTo);
+
+                // set subject and encoding
+                myMail.Subject = "Test message";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // set body-message and encoding
+                myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                  ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 	}
 }
--- Projeto Hospital/SQLQuery1.sql
+++ Projeto Hospital/SQLQuery1.sql
@@ -3,24 +3,13 @@ Idcargo int identity(1,1) primary key,
 cargo varchar(100),
 )
 
-Insert into tblCargo(cargos) values('Secretaria/o')
-Insert into tblCargo(cargos) values('Enfermeira/o')
-Insert into tblCargo(cargos) values('Ciurgia/ão')
-Insert into tblCargo(cargos) values('Medica/o')
-Insert into tblCargo(cargos) values('Psicologa/o')
-Insert into tblCargo(cargos) values('Fisioterapeuta')
-Insert into tblCargo(cargos) values('SemCargo')
-
-Create table tblDoutor(
-IdDoutor int identity(1,1) primary key,
-nOrdemDosMedicos int not null,
-nome varchar(350),
-IdCargo int Foreign key references tblcargo(Idcargo),
-horasTrabalhadas datetime not null,
-primeiravez date not null, -- Primeira vez que começou a trabalhar no hospital
-inicioTrabalho datetime, -- primeira vez que fez login
-fimdetrabalho datetime, -- ultimo login do dia
-)
+Insert into tblCargo(cargo) values('Secretaria/o')
+Insert into tblCargo(cargo) values('Enfermeira/o')
+Insert into tblCargo(cargo) values('Ciurgia/ão')
+Insert into tblCargo(cargo) values('Medica/o')
+Insert into tblCargo(cargo) values('Psicologa/o')
+Insert into tblCargo(cargo) values('Fisioterapeuta')
+Insert into tblCargo(cargo) values('SemCargo')
 
 
 Create table tblUser(
@@ -28,12 +17,24 @@ IdUser int identity(1,1) primary key,
 nomecompleto varchar(350) not null,
 sexo varchar(1) not null,
 altura float not null,
-nutentedesaude varchar(9) not null
+nutentedesaude varchar(9) not null,
 icon image,
 Nascimento date not null,
 telemovel varchar(9) not null,
 fichatecnica varchar(max)  not null,
-IdMedicoFamilia int Foreign key references tblDoutor(IdDoutor) not null
+IdMedicoFamilia int
+)
+
+
+Create table tblDoutor(
+IdDoutor int identity(1,1) primary key,
+idUser int Foreign Key references tblUser(idUser),
+nOrdemDosMedicos int not null,
+IdCargo int Foreign key references tblcargo(Idcargo),
+horasTrabalhadas int not null,
+primeiravez date not null, -- Primeira vez que começou a trabalhar no hospital
+inicioTrabalho datetime, -- primeira vez que fez login
+fimdetrabalho datetime, -- ultimo login do dia
 )
 
 Create table tblLogin(
@@ -51,8 +52,26 @@ img1 image not null,
 img2 image,
 img3 image,
 img4 image,
-linkvideo varchar(100)
+linkvideo varchar(100),
 data datetime not null,
 autor varchar(70) not null,
 categoria varchar(250) not null,
 )
+
+insert tblUser([nomecompleto],[sexo],[altura],[nutentedesaude],[icon],[Nascimento]      ,[telemovel]
+      ,[fichatecnica],[IdMedicoFamilia]) values('Pedro Manuel da Silva', 'M', 1.80, 
+	  50001, '', '2001-02-04T10:00:00.000Z', 999999999, 'Asma', 500)
+
+insert tblUser([nomecompleto],[sexo],[altura],[nutentedesaude],[icon],[Nascimento]      ,[telemovel]
+      ,[fichatecnica],[IdMedicoFamilia]) values('Maria Isabel', 'F', 1.65, 
+	  50002, '', '1945-08-10T10:00:00.000Z', 999999991, '---', 500)
+
+insert tblUser([nomecompleto],[sexo],[altura],[nutentedesaude],[icon],[Nascimento]      ,[telemovel]
+      ,[fichatecnica],[IdMedicoFamilia]) values('Marta Marquês', 'F', 1.60, 
+	  40002, '', '1965-02-15T10:00:00.000Z', 999999992, '---', 500)
+
+insert tbldoutor([idUser], [nOrdemDosMedicos], [IdCargo], [horasTrabalhadas], [primeiravez], [inicioTrabalho], [fimdetrabalho])
+  values (1, 500, 4, 0, '2020-02-04T10:00:00.000Z', '2023-06-30T07:00:00.000Z', '2023-06-30T20:00:00.000Z')
+  insert tbldoutor([idUser], [nOrdemDosMedicos], [IdCargo], [horasTrabalhadas], [primeiravez], [inicioTrabalho], [fimdetrabalho])
+  values (3, 4, 0, '2020-02-04T10:00:00.000Z', '2023-06-30T07:00:00.000Z', '2023-06-30T20:00:00.000Z') 
+
